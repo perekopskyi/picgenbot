591ffb8951b9b509a060f07b02d6406b0773f824
i_am_yevhen <yevheniiperekopskyi@gmail.com>
2020-05-12 10:48:04 +0300

    first commit

---
+++ bot.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN); // t.me/TestovOlegThe_bot
+bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name} !`));
+bot.help((ctx) => ctx.reply('Отправь мне стикер'));
+bot.on('sticker', (ctx) => ctx.reply('👍 Каеф)'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
