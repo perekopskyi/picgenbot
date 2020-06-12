9a4321830aea452500eae800b7f7d03c0008d1d5
i_am_yevhen <yevheniiperekopskyi@gmail.com>
2020-06-12 12:45:06 +0300

    Добавил ответы на вопросы

--- bot.js
+++ bot.js
@@ -2,10 +2,26 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
 bot.start((ctx) =>
-  ctx.reply(`Привет, ${ctx.message.from.first_name} ! Введи /help чтобы узнать что я умею`)
+  ctx.reply(`
+  Привет, ${ctx.message.from.first_name}!
+Введи /help чтобы узнать что я умею`)
 );
-bot.help((ctx) => ctx.reply('Отправь мне стикер'));
+
+bot.help((ctx) =>
+  ctx.reply(`
+❓ Отвечаю на вопросы "где?", "когда?", "кто?"
+или отправь мне стикер 😉`)
+);
+
 bot.on('sticker', (ctx) => ctx.reply('👍 Каеф)'));
+
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.hears('где?', (ctx) =>
+  ctx.reply(`🏖 Вейк Парк "Голубое Озеро" https://goo.gl/maps/2kgUQqjBd5VQVo537`)
+);
+bot.hears('когда?', (ctx) => ctx.reply('📆 Суббота, 27 июня 2020'));
+bot.hears('кто?', (ctx) => ctx.reply('Именинница 🥳, ты и еще несколько друзей 😁'));
+
 bot.launch();
