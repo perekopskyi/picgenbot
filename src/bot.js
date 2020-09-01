b23276e2fda3c53034c6777eb0c4732e5f2a9a20
Yevhenii Perekopskyi <yevheniiperekopskyi@gmail.com>
2020-09-01 15:52:08 +0300

    Add src folder

---
+++ src/bot.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) =>
+  ctx.reply(`
+  Привет, ${ctx.message.from.first_name}!
+Введи /help чтобы узнать что я умею`)
+);
+
+bot.help((ctx) =>
+  ctx.reply(`
+❓ Отвечаю на вопросы "Где?", "Когда?", "Кто?"
+или отправь мне стикер 😉`)
+);
+
+bot.on('sticker', (ctx) => ctx.reply('👍 Каеф)'));
+
+bot.hears('Где?', (ctx) =>
+  ctx.reply(`🏖 Вейк Парк "Голубое Озеро" https://goo.gl/maps/2kgUQqjBd5VQVo537`)
+);
+bot.hears('Когда?', (ctx) => ctx.reply('📆 Суббота, 27 июня 2020'));
+bot.hears('Кто?', (ctx) => ctx.reply('Именинница 🥳, ты и еще несколько друзей 😁'));
+
+export default bot;
